a6e3f0058864552ba7885e770c1837692f2d49ab
vini-haa <vinicius.andrade@somosicev.com>
2025-03-27 11:45:14 -0300

    subindo todo o app

---
+++ android/app/build/intermediates/cxx/Debug/5j2p4a1z/logs/x86_64/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"C:\\Users\\vhand\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\flutter\\flutter\\packages\\flutter_tools\\gradle\\src\\main\\groovy" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86_64" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86_64" ^
+  "-DANDROID_NDK=C:\\Users\\vhand\\AppData\\Local\\Android\\sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\vhand\\AppData\\Local\\Android\\sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\vhand\\AppData\\Local\\Android\\sdk\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\vhand\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\vhand\\Downloads\\docs\\docs\\faculdade\\dart\\finance\\financy_app_new\\android\\app\\build\\intermediates\\cxx\\Debug\\5j2p4a1z\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\vhand\\Downloads\\docs\\docs\\faculdade\\dart\\finance\\financy_app_new\\android\\app\\build\\intermediates\\cxx\\Debug\\5j2p4a1z\\obj\\x86_64" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\vhand\\Downloads\\docs\\docs\\faculdade\\dart\\finance\\financy_app_new\\android\\app\\.cxx\\Debug\\5j2p4a1z\\x86_64" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli
